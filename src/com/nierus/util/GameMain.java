2772ce7729543d78fe039de9836eb97d2c7db8cf
Sunyboy_FL <1171128558@qq.com>
2021-06-26 10:25:49 +0800

    first push

---
+++ src/com/nierus/util/GameMain.java
@@ -0,0 +1,13 @@
+package com.nierus.util;
+
+/**
+ * @ClassName: GameMain
+ * @Description: TODO
+ * @author: Nierus
+ * @date: 2021年06月26日 10:19
+ */
+public class GameMain {
+    public static void main(String [] args){
+
+    }
+}
